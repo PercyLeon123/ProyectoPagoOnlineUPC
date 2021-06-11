31ab7daeee01cf23f153b1fe0abd124098cc7da6
Percy Leon <percyleon107@gmail.com>
2021-06-10 21:17:47 -0500

    Componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagoPendienteComponent } from './pages/pago-pendiente/pago-pendiente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:PagoPendienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
