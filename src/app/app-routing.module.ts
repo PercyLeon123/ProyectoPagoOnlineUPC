10d43423f63e5bf29313ea07da76088c03668bed
Percy Leon <percyleon107@gmail.com>
2021-06-10 22:39:02 -0500

    Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagoPendienteComponent } from './pages/pago-pendiente/pago-pendiente.component';
+import { PagoOnlineComponent } from './pages/pago-online/pago-online.component';
 
 const routes: Routes = [
-  {path:'', component:PagoPendienteComponent}
+  {path:'', component:PagoPendienteComponent},
+  {path:'PagoPendiente', component:PagoPendienteComponent},
+  {path:'PagoOnline', component:PagoOnlineComponent}
 ];
 
 @NgModule({
