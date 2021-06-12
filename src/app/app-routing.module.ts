57d1852a002d52fe799b986ab8f739c90981ce72
Percy Leon <percyleon107@gmail.com>
2021-06-11 22:13:03 -0500

    Pruebas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagoPendienteComponent } from './pages/pago-pendiente/pago-pendiente.component';
 import { PagoOnlineComponent } from './pages/pago-online/pago-online.component';
+import { HojaComponent } from './pages/hoja/hoja.component';
 
 const routes: Routes = [
   {path:'', component:PagoPendienteComponent},
   {path:'PagoPendiente', component:PagoPendienteComponent},
-  {path:'PagoOnline', component:PagoOnlineComponent}
+  {path:'PagoOnline', component:PagoOnlineComponent},
+  {path:'Hoja', component:HojaComponent}
 ];
 
 @NgModule({
