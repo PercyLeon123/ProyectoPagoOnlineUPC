a7d863ba44a2885424203610b87d2f240cc7583a
mafernove <u202015565@upc.edu.pe>
2021-06-13 07:53:13 -0500

    up

--- src/app/pages/restablecercontasena/restablecercontasena.component.ts
+++ src/app/pages/restablecercontasena/restablecercontasena.component.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 import { Component,OnInit } from '@angular/core';
-=======
-import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
->>>>>>> eb307b8a29242b2a7d2b620aba36346eb45e2d29
 
 @Component({
   selector: 'app-restablecercontasena',
@@ -12,21 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restablecercontasena.component.css']
 })
 export class RestablecercontasenaComponent implements OnInit {
-<<<<<<< HEAD
-  RestableceForm: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
-=======
-
   RestableceForm=this.fb.group ({
     dni: ['', Validators.required],
     usuario: ['', Validators.required]
   });
 
   constructor(private fb:FormBuilder) { }
-
->>>>>>> eb307b8a29242b2a7d2b620aba36346eb45e2d29
-  onSubmit() {
+ onSubmit() {
     if(this.RestableceForm.valid){
     }else{
       alert("DNI o Usuario no valido");
