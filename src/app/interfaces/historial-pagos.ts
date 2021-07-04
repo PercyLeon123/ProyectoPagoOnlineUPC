b84589ca2e1ddc9bd68e20ffc20a3715bb89d1c6
juliofaustor <julio.faustor@gmail.com>
2021-07-04 12:33:46 -0500

    Maqueteo historial eecta

--- src/app/interfaces/historial-pagos.ts
+++ src/app/interfaces/historial-pagos.ts
@@ -1,12 +1,14 @@
 export interface historialpago{
-  Anio:string;
-  Vencimiento:string;
-  Fechapago:string;
-  NroOperacion:string;
-  Transaccion:string;  
+  nperiodo:string;
+  fecha_vcmto:string;
+  fecha_pago:string;
+  Nro_ope:string;
+  nro_tran:string;  
   Fact:string;
-  afrp1:string;
-  desafrp1:string;
-  ImporteDeuda:string;
-  Fecha:string;
+  afpr1:string;
+  des_afr1:string;
+  imp_deuda:string;  
+  code:string;
+  fracciona:string;
+  des_fracciona : string;
 }
--- src/app/pages/historial-pago/historial-pago.component.ts
+++ src/app/pages/historial-pago/historial-pago.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute,Params } from '@angular/router';
 import { HistorialpagoService } from 'src/app/services/historialpago.service';
 import { NzTableFilterFn, NzTableFilterList, NzTableSortFn, NzTableSortOrder } from 'ng-zorro-antd/table';
 
+import { historialpago } from 'src/app/interfaces/historial-pagos';
+
 import { textSpanIsEmpty } from 'typescript';
 
 
@@ -26,6 +28,7 @@ historialpagos = []
    this.historialpagoService.gethistorial(idcontr,fdesde,fhasta).subscribe((Res : any)=>{
 //   this.historialpago =Res;
    this.historialpagos = Res;
+   
    console.log(this.historialpagos);
 
 }
@@ -40,6 +43,91 @@ historialpagos = []
       }
     })
     
+  }
+
+  searchValue = '';
+  visible = false;
+
+  listOfColumn = [
+    {
+      title: 'Periodo',
+      compare: (a: historialpago, b: historialpago) => a.nperiodo.localeCompare(b.nperiodo),
+      priority: 2
+    },
+
+    {
+      title: 'Fecha Vcmto',
+      compare: (a: historialpago, b: historialpago) => a.fecha_vcmto.localeCompare(b.fecha_vcmto),
+      priority: 2
+    },
+
+    
+    {
+      title: 'Fecha de Pago',
+      compare: (a: historialpago, b: historialpago) => a.fecha_pago.localeCompare(b.fecha_pago),
+      priority: 2
+    },
+
+    {
+      title: 'Nro Operación',
+      compare: (a: historialpago, b: historialpago) => a.Nro_ope.localeCompare(b.Nro_ope),
+      priority: 2
+    },
+
+    {
+      title: 'Nro Tran',
+      compare: (a: historialpago, b: historialpago) => a.nro_tran.localeCompare(b.nro_tran),
+      priority: 2
+    },
+
+     {
+      title: 'Fact',
+      compare: (a: historialpago, b: historialpago) => a.Fact.localeCompare(b.Fact),
+      priority: 2
+    },
+    
+    {
+      title: 'Afpr',
+      compare: (a: historialpago, b: historialpago) => a.afpr1.localeCompare(b.afpr1),
+      priority: 2
+    },
+    {
+      title: 'Desafr1',
+      compare: (a: historialpago, b: historialpago) => a.des_afr1.localeCompare(b.des_afr1),
+      priority: 2
+    },
+    {
+      title: 'Code',
+      compare: (a: historialpago, b: historialpago) => a.code.localeCompare(b.code),
+      priority: 1
+    },
+    {
+      title: 'Fraciona',
+      compare: (a: historialpago, b: historialpago) => a.fracciona.localeCompare(b.fracciona),
+      priority: 2
+    },
+    {
+      title: 'Desfraccion',
+      compare: (a: historialpago, b: historialpago) => a.des_fracciona.localeCompare(b.des_fracciona),
+      priority: 1
+    },
+  
+    {
+      title: 'Importe Deuda',
+      compare: (a: historialpago, b: historialpago) => a.imp_deuda.localeCompare(b.imp_deuda),
+      priority: 1
+    }
+    
+  ];
+  listOfData: historialpago[] =  this.historialpagos;
+  listOfDisplayData = [...this.listOfData];
+  reset(): void {
+    this.searchValue = '';
+    this.search();
+  }
+  search(): void {
+    this.visible = false;
+    this.listOfDisplayData = this.listOfData.filter((item: historialpago) => item.afpr1.indexOf(this.searchValue) !== -1);
   }
 
 }
